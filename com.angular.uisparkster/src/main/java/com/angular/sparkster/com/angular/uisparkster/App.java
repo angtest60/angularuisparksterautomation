8d106637038bdd671b6311fd545a14f34b9e644f
vedvyas.pb <vedvyas.pb@ui1.bov3.com>
2019-08-26 14:40:27 +0530

    changes on xml file.download the latest jars

---
+++ com.angular.uisparkster/src/main/java/com/angular/sparkster/com/angular/uisparkster/App.java
@@ -0,0 +1,13 @@
+package com.angular.sparkster.com.angular.uisparkster;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
